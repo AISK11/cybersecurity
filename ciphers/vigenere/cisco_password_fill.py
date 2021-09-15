1043fb8a721d3c228f0cb1a80775735b51ea72d5
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-09-15 15:01:12 +0200

    Add files via upload

--- ciphers/vigenere/cisco_password_fill.py
+++ ciphers/vigenere/cisco_password_fill.py
@@ -16,10 +16,31 @@ import os
 
 def main():
 	# Do password encryption:
-	print("enable")
-	print("conf t")
-	print("service password-encryption")
-	print("end\n\n")
+	OUTPUT = []
+	OUTPUT.append("!")
+	OUTPUT.append("enable")
+	OUTPUT.append("conf t")
+	OUTPUT.append("service password-encryption")
+	OUTPUT.append("end")
+	OUTPUT.append("!")
+	OUTPUT.append("!")
+	
+	# this variable contains one big string properly formatted from list variable "OUTPUT"
+	OUTPUT_CLEAN = ""
+	output_i = 0
+	while output_i < len(OUTPUT):
+		OUTPUT_CLEAN += OUTPUT[output_i] + "\n"
+		output_i += 1	
+		
+	# print "OUTPUT_CLEAN" to stdout 
+	print(f"{OUTPUT_CLEAN}")
+		
+	# print "OUTPUT_CLEAN" alsto to xclip (linux X clipboard)
+	os.system(f'echo -n "{OUTPUT_CLEAN}" | xclip -selection clipboard') 
+	os.system(f'echo -n "{OUTPUT_CLEAN}" | xclip -selection primary')
+
+	# wait for an input, so it prints one set of chars at a time
+	input()
 
 	# (writable) characters to use to set a password, range: 0x20 = ' ', 0x7E = '~'
 	# loop through all characters
@@ -32,7 +53,7 @@ def main():
 		OUTPUT.append("line console 0")
 		OUTPUT.append(f"password {20 * chr(i)}")
 		OUTPUT.append("end")
-		OUTPUT.append("show run | i password 7")
+		OUTPUT.append("show run | i password")
 		OUTPUT.append("!")
 		OUTPUT.append("!")
 		
