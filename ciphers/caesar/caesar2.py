3d6c01483286b2af10f1fcde374927ec3818c8ef
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-09-16 01:19:40 +0200

    Add files via upload

--- ciphers/caesar/caesar2.py
+++ ciphers/caesar/caesar2.py
@@ -16,8 +16,8 @@ def main():
 
 	# mutually exclusive arguments:
 	crypt = parser.add_mutually_exclusive_group()
-	crypt.add_argument("-d", "--decrypt", action="store_true", default=True, help="decryption - shift letters up X characters (DEFAULT)")
-	crypt.add_argument("-e", "--encrypt", action="store_true", default=False, help="encryption - shift letters down X characters")
+	crypt.add_argument("-d", "--decrypt", action="store_true", default=True, help="decryption - shift letters <<< by X characters (DEFAULT)")
+	crypt.add_argument("-e", "--encrypt", action="store_true", default=False, help="encryption - shift letters >>> by X characters")
 	# shift argument:
 	parser.add_argument("-s", "--shift", default="3", help="specify X, when X is a numerical value or range used to shift letters (DEFAULT 3)")
 	# mutually exclusive arguments:
@@ -29,17 +29,40 @@ def main():
 	text.add_argument("-t", "--text", action="store_true", default=True, help="cipher should be read from stdin (DEFAULT)")
 	text.add_argument("-f", "--file", action="store_true", default=False, help="cipher should be read from file")
 	# positional argument: 
-	parser.add_argument("CIPHER", help="specify cipher or file from which read cipher")
+	parser.add_argument("MESSAGE", help="specify message or file from which text/cipher will be read")
 
 	# pass arguments
 	args = parser.parse_args()
 	
-	
-	
-	num_list = shiftRange(args.shift)
-	print(f"{num_list}")
+
+	# return list filled with numbers according to specified range:
+	shift_range = shiftRange(args.shift)
 
 
+	# check if encrypt flag was set:
+	decrypt = 1
+	if args.encrypt:
+		decrypt = 0
+
+	# check if file flag was set and return list full of outputs to "output":
+	if args.file:
+		output = processFile(decrypt, shift_range, args.MESSAGE)
+	else:
+		output = processMessage(decrypt, shift_range, args.MESSAGE)
+
+
+	# loop through output list:
+	i = 0
+	while i < len(shift_range):
+		if args.quiet:
+			print(f"{output[i]}\n")
+		else:
+			if decrypt:
+				print(f"[*] DECRYPTED (using {shift_range[i]} shifts):")
+			else:
+				print(f"[*] ENCRYPTED (using {shift_range[i]} shifts):")
+			print(f"{output[i]}\n")
+		i += 1
 
 
 	# exit program with return code 0:
@@ -96,7 +119,7 @@ def shiftRange(shiftRange):
 			y = num_list[i+1]
 			# check if X is < than Y:
 			if x >= y:
-				print(f"[!] ERROR! Number '{x}' must be smaller than '{y}!")
+				print(f"[!] ERROR! Number '{x}' must be smaller than '{y}!", file=sys.stderr)
 				sys.exit(4)
 			# while X < Y:
 			while x < y:
@@ -123,6 +146,70 @@ def shiftRange(shiftRange):
 
 	# return clean "num_list"
 	return num_list
+
+
+def processFile(decrypt, shift_range, input_file):
+	# check if file "input_file" exists:
+	if not os.path.exists(input_file):
+		print(f"[!] ERROR! File '{input_file}' does not exists!", file=sys.stderr)
+		sys.exit(5)
+	else:
+		# open file
+		fo = open(input_file, "r+")
+
+		# read whole file to variable "line"
+		line = fo.read(-1)
+
+		# close file
+		fo.close()
+
+	#process file content:
+	return processMessage(decrypt, shift_range, line)
+
+
+def processMessage(decrypt, shift_range, message):
+	message_processed = ""
+
+	# negative decryption is encryption:
+	if decrypt == 0:
+		i = 0 
+		while i < len(shift_range):
+			shift_range[i] *= -1
+			i += 1
+
+	output_list = [] # as multiple ranges can be decrypted, each answer will be an item in "output_list"
+	output_index = 0 # first decryption 
+	while output_index < len(shift_range): 
+		shift_number = shift_range[output_index] # current shift which will be used for decryption from "shift_range"
+		# decrypt/encrypt the message:
+		i = 0 
+		while i < len(message):
+			if message[i] >= 'A' and message[i] <= 'Z':
+				# convert chars to ints so math can be performed:
+				if ord(message[i]) - shift_number < ord('A'):
+					message_processed += chr(ord(message[i]) - shift_number + 26) 
+				# negative decryption (encryption):
+				elif ord(message[i]) - shift_number > ord('Z'):
+					message_processed += chr(ord(message[i]) - shift_number - 26) 
+				else:
+					message_processed += chr(ord(message[i]) - shift_number)
+			elif message[i] >= 'a' and message[i] <= 'z':
+				# convert chars to ints so math can be performed:
+				if ord(message[i]) - shift_number < ord('a'):
+					message_processed += chr(ord(message[i]) - shift_number + 26) 
+				# negative decryption (encryption):
+				elif ord(message[i]) - shift_number > ord('z'):
+					message_processed += chr(ord(message[i]) - shift_number - 26) 
+				else:
+					message_processed += chr(ord(message[i]) - shift_number)
+			else:
+				message_processed += message[i]
+			i += 1
+		output_list.append(message_processed)
+		message_processed = ""
+		output_index += 1
+
+	return output_list
 
 
 main()
